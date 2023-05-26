85ed6853e6327319a15fdffa975f4bc201ef70b1
Gaurav <chaudharygaurav424@gmail.com>
2023-05-26 09:29:03 +0530

    mini project done

--- src/jframe/DBConnection.java
+++ src/jframe/DBConnection.java
@@ -6,23 +6,22 @@ package jframe;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-
+import java.sql.SQLException;
 
 /**
  *
  * @author Gaurav
  */
 public class DBConnection {
-    
+
     static Connection con = null;
-        
-    public static Connection getConnection(){
-            try {
-                Class.forName("com.mysql.jdbc.Driver");
-                 con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_ms", "root", "");
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return con;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/library_ms", "root", "");
+        } catch (ClassNotFoundException | SQLException e) {
         }
+        return con;
     }
+}
